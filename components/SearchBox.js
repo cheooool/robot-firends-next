92a06dc887e645dd7398dd3f6eb8a25fdb8731b0
g.cheooool <g.cheooool@gmail.com>
2019-04-15 18:53:14 +0900

    SearchBox 추가

---
+++ components/SearchBox.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyleBox = styled.div`
+  margin: 1em 0;
+`;
+
+const SearchField = styled.input`
+  display: block;
+  width: 100%;
+  max-width: 640px;
+  margin: 0 auto;
+  padding: 0.5em 1em;
+  font-size: 1.5em;
+  text-align: center;
+  border: 1px solid #eaeaea;
+  background: transparent;
+  box-sizing: border-box;
+`;
+
+const SearchBox = () => {
+  return (
+    <StyleBox>
+      <SearchField type="text" placeholder="Search Robot" />
+    </StyleBox>
+  );
+};
+
+export default SearchBox;
