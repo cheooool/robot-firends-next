d8932ac834c062c56279152dbbab479f9694e9a6
g.cheooool <g.cheooool@gmail.com>
2019-04-15 19:35:51 +0900

    Title 스타일 추가

--- layouts/template.js
+++ layouts/template.js
@@ -5,6 +5,14 @@ const Header = styled.header`
   padding: 1em;
   text-align: center;
 `;
+const Title = styled.h1`
+  margin: 0;
+  font-size: 4em;
+  font-family: 'SEGA LOGO FONT';
+  font-weight: 200;
+  line-height: 1.5em;
+  color: #0ccac4;
+`;
 const Container = styled.div`
   padding: 1em;
 `;
@@ -12,7 +20,9 @@ const Container = styled.div`
 const Template = ({ title, children }) => {
   return (
     <>
-      <Header>{title}</Header>
+      <Header>
+        <Title>{title}</Title>
+      </Header>
       <Container>{children}</Container>
     </>
   );
